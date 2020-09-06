2f63eb84fbfe78fc36ffd4bc279ab9170bad90b8
Silvia Hernandez <silviaherf@gmail.com>
2020-09-06 15:13:12 +0200

    Nueva funcion para redondear a 2 decimales y jupyters actualizados

--- src/cleaning.py
+++ src/cleaning.py
@@ -1,7 +1,20 @@
 import regex as re
 def rename_columns(df):
+    """ 
+    This function checks the name of the columns in a DataFrame and replace blank spaces with "_".
+    The only argument needed is df:the DataFrame
+    """
     actual_col={}
     for column in df.columns:
         actual_col[column]=column.replace(r" ","_")
     df=df.rename(columns = actual_col, inplace = True)
+    return df
+
+def rounding(df):
+    """
+    This function rounds every numerical column in a DataFrame to 2 decimals.
+    The only argument needed is df:the DataFrame
+    """
+    for column in df.select_dtypes(include=['int','float']).columns:
+        df[column]=df[column].map(lambda x:round(x,2))
     return df
