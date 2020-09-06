0fcecbf493e16a9092861abd3f67e1df27d3cf09
Silvia Hernandez <silviaherf@gmail.com>
2020-09-06 12:53:05 +0200

    nueva funcion open_df y cambios en emissions_analysis

--- src/manipulating_df.py
+++ src/manipulating_df.py
@@ -1,9 +1,20 @@
+
+import pandas as pd
+
 def export_cvs(df):
     """ This function export a cvs file to src folder in my project.
-    To use it, it needs two parameters:
+    To use it, it needs one parameter:
     df:the DataFrame we are willing to export to cvs
-    name:the name for the cvs file
     """
     name=input('Please, enter the name for the cvs file you want to export')
-    df.to_csv(f"src/{name}.csv")
+    df.to_csv(f"src/{name}.csv",index = False)
     return f"{name} has been exported to cvs file"
+
+def open_cvs():
+    """ This function opens a cvs file from src folder in my project. and saves it into a pandas DataFrame
+    No parameters are needed, except of the name the function will ask you
+    """
+    name=input('Please, enter the name for the cvs file you want to open without extension:\n')
+    
+
+    return pd.read_csv(f'src/{name}.csv',encoding='latin-1')
